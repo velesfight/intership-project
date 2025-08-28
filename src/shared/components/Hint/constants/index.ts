fe9fffc2651debaf47809c33bc15d0aa545b390a
Инекчян Андраник Андраникович <andranica@test.com>
2025-08-28 13:56:11 +0000

    revert: hint
    **Ссылка на таск**
    
    
    **Описание выполненной работы**
    
    убираю неправильно запушенный компонент Hint
    
    !28

--- src/shared/components/Hint/constants/index.ts
+++
@@ -1,6 +0,0 @@
-import ErrorIcon from '~/shared/assets/icons/error.svg';
-import SuccessIcon from '~/shared/assets/icons/success.svg';
-import WarningIcon from '~/shared/assets/icons/warning.svg';
-
-export { ErrorIcon, SuccessIcon, WarningIcon };
-export const DEFAULT_VARIANT: string = 'default';
